b8e74a91c69cf2d9747be0d89c9e89e401ff4293
Hoang Luc <lucnhhps25674@fpt.edu.vn>
2022-03-14 21:38:24 +0700

    Lab4

--- HomeWork/Lab4/Lab4_3.c
+++ HomeWork/Lab4/Lab4_3.c
@@ -12,9 +12,10 @@ void sochinhphuong(){
     for (int i = 1; i <= n; i++){
         if(i * i == n){
             count++;
+            break;
         } 
     }
     if (count == 1){
-            printf("%d la so chinh phuong",n);
-    } printf("%d khong phai la so chinh phuong",n);
+        printf("%d la so chinh phuong",n);
+    } else printf("%d khong phai la so chinh phuong",n);
 }
