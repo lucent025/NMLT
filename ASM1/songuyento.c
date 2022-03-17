e6fc500fed9bc0759b2d33ecd4e5d8fdbe05ce93
Hoang Luc <lucnhhps25674@fpt.edu.vn>
2022-03-17 13:20:39 +0700

    ASM chuc nang so 1

---
+++ ASM1/songuyento.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    songuyento();
+    return 0;
+}
+
+void songuyento(){
+    int n, biendem = 0;
+    printf("Nhap vao so n: "); scanf("%d",&n);
+
+    for (int i = 2; i < n; i++){
+        if (n % i == 0){
+            biendem++;
+        }
+    }
+    if (biendem == 0){
+        printf("\n%d la so nguyen to!",n);
+    } else  {
+        printf("\n%d khong phai la so nguyen to!\n",n);
+    } 
+}
