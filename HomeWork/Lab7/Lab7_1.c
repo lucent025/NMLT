0ca4ce0b0915a375547c0ccc427c2f43b6c05924
Hoang Luc <lucnhhps25674@fpt.edu.vn>
2022-03-23 09:51:11 +0700

    Lab7_1

---
+++ HomeWork/Lab7/Lab7_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void dempnam();
+
+int main(){
+    dempnam();
+    return 0;
+}
+
+void dempnam(){
+    char string[6];
+    printf("Xin moi nhap vao chuoi: "); gets(string);
+    int i = 0, p = 0, n = 0;
+
+    while(string[i] != '\0'){
+       if (string[i] == 'u' || string[i] == 'e' || string[i] == 'o' ||
+       string[i] == 'a' || string[i] == 'i'){
+           n++;
+       } else {p++;} 
+       i++;
+    }
+    printf("Chuoi '%s' co %d nguyen am va %d phu am!",string,n,p);
+}
