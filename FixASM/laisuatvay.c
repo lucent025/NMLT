91a30ddfbe4d52d541c8264c60425a3c9710e8a5
lucent025 <smile25003@gmail.com>
2022-04-12 12:32:20 +0700

    update

--- FixASM/laisuatvay.c
+++ FixASM/laisuatvay.c
@@ -6,20 +6,22 @@ int tinhlai();
 
 int sotien;
 void laisuatvay(){
-    printf("Nhap vao so tien muon vay: "); scanf("%d",&sotien);
+    printf("Nhap vao so tien muon vay : "); scanf("%d",&sotien);
     tinhlai();
 }
 
 int tinhlai(){
-    int i,*lai[]     = {0,0,0,0,0,0,0,0,0,0,0,0}
-         ,*conlai[]  = {0,0,0,0,0,0,0,0,0,0,0,0}
-         ,*phaitra[] = {0,0,0,0,0,0,0,0,0,0,0,0};
+    int i,lai[]     = {0,0,0,0,0,0,0,0,0,0,0,0}
+         ,conlai[]  = {0,0,0,0,0,0,0,0,0,0,0,0}
+         ,phaitra[] = {0,0,0,0,0,0,0,0,0,0,0,0};
     int goc = sotien/12;
+    int temp;
     for(i = 0; i < 12;i++){
         lai[i] = (int)(sotien*0.05);
         sotien = sotien - goc;
-        *conlai[i] = sotien;
-        phaitra[i] = lai[i]+goc;
+        conlai[i] = sotien;
+        temp = lai[i]+goc;
+        phaitra[i] = temp;
     }
     printf("%d\n",phaitra[i]);
     printf("|   Ky Han   |    Lai Phai Tra    |     Goc Phai Tra     |     So tien Phai Tra     |     So tien Con lai     |\n");
@@ -27,3 +29,4 @@ int tinhlai(){
     setlocale(LC_NUMERIC, "");
         printf("     %'d              %'d              %'d               %'d                  %'d           \n",j+1,lai[j],goc,phaitra[j],conlai[j]);
     }
+}
